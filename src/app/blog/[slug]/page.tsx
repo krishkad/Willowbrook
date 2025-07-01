69fa379bcc959dfd43fbcc25334fe2d5ecf368c6
krrish <krishkadam88@gmail.com>
2025-07-01 17:09:25 +0530

    sixth commit

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,36 @@
+// import BlogPost from "@/components/BlogPost";
+// import { blogPosts } from "@/data/BlogData";
+// import React from "react";
+
+// interface PageProps {
+//   params: { slug: string };
+// }
+
+// async function BlogPostPage({ params }: PageProps) {
+//   const { slug } = params;
+//   return <BlogPost slug={slug} />;
+// }
+
+// export async function generateStaticParams() {
+//   return blogPosts.map((post) => ({
+//     slug: post.slug,
+//   }));
+// }
+
+// export default BlogPostPage;
+
+
 import BlogPost from "@/components/BlogPost";
 import { blogPosts } from "@/data/BlogData";
 import React from "react";
 
-interface PageProps {
+interface BlogPageProps {
   params: { slug: string };
 }
 
-async function BlogPostPage({ params }: PageProps) {
+async function BlogPostPage({ params }: BlogPageProps) {
   const { slug } = params;
+
   return <BlogPost slug={slug} />;
 }
 
@@ -17,4 +40,8 @@ export async function generateStaticParams() {
   }));
 }
 
+export const dynamicParams = true; // Optional, ensures [slug] is treated dynamically
+
 export default BlogPostPage;
+
+
